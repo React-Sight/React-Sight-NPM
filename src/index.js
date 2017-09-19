ee056b8f503821dee4c13af14dc5406ca51f1030
Grant Kang <g1k05j@gmail.com>
2017-09-19 15:31:16 -0700

    updated traversal method

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import God from '../Component/God'
+
+const rootEl = document.getElementById('groot')
+
+ReactDOM.render(
+  <God />, rootEl
+)
